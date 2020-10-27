f0a15a11ca95839e559b46ae48ce7b29507781f5
IlyaLitvin <darnon88@gmail.com>
2020-10-27 15:54:41 +0200

    new

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const colors = [
-  '#FFFFFF',
-  '#2196F3',
-  '#4CAF50',
-  '#FF9800',
-  '#009688',
-  '#795548',
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
 ];
 
 const start = document.querySelector('[data-action = "start"]');
-const start = document.querySelector('[data-action = "stop"]');
-const body = document.querySelector('body');
+const stop = document.querySelector('[data-action = "stop"]');
+const body = document.querySelector("body");
 let intervalId;
-
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+const startChange = function () {
+  intervalId = setInterval(() => {
+    start.setAttribute("disabled", true);
+    const min = 0;
+    const max = colors.length - 1;
+    let i = randomIntegerFromInterval(min, max);
+    body.style.backgroundColor = colors[i];
+  }, 1000);
+};
+const stopChange = function () {
+  clearInterval(intervalId);
+  start.removeAttribute("disabled");
+};
+
+start.addEventListener("click", startChange);
+stop.addEventListener("click", stopChange);
